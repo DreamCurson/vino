13ce93752f47cb05e37aae2e34b18dd151378284
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2026-04-09 20:30:45 -0400

    ajout message alert components

--- caveo/resources/views/layouts/main.blade.php
+++ caveo/resources/views/layouts/main.blade.php
@@ -70,6 +70,22 @@
         </div>
     </footer>
     @endif
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const messages = document.querySelectorAll('.message-flash-auto');
+
+            messages.forEach(function(message) {
+                setTimeout(function() {
+                    message.classList.add('opacity-0', 'transition-opacity', 'duration-500');
+
+                    setTimeout(function() {
+                        message.remove();
+                    }, 500);
+                }, 3000); // ⬅️ 3 secondes
+            });
+        });
+    </script>
 </body>
 
 </html>
