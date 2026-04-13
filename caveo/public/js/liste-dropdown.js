67ae63caf4edd8d8135ee13f2997a18e9d2e342f
DreamCurson <dreamcurson@outlook.com>
2026-04-13 15:10:26 -0400

    Modification du script pour aller chercher toggle-list

--- caveo/public/js/liste-dropdown.js
+++ caveo/public/js/liste-dropdown.js
@@ -1,9 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".toggle-liste").forEach((button) => {
-        button.addEventListener("click", () => {
-            const target = document.getElementById(button.dataset.target);
+document.querySelectorAll(".toggle-liste").forEach((el) => {
+    el.addEventListener("click", (e) => {
+        if (e.target.closest("a, button, form")) return;
 
-            target.classList.toggle("hidden");
-        });
+        const target = document.getElementById(el.dataset.target);
+        target.classList.toggle("hidden");
     });
 });
