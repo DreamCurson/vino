441df02ad6d87dd5c252a888118e8add6ac6cef2
DreamCurson <dreamcurson@outlook.com>
2026-04-21 12:02:32 -0400

    ajout des query pour les garder, ajout alerte

--- caveo/resources/views/catalogue/index.blade.php
+++ caveo/resources/views/catalogue/index.blade.php
@@ -142,7 +142,7 @@
     </div>
 </form>
 
-<div class="m-4">
+<div class="m-4 message-flash-auto">
     <x-alerts />
 </div>
 
@@ -374,10 +374,10 @@
 
                     </div>
 
-                    <input type="hidden" name="quantite" id="cellierQuantite" value="1">
                 </div>
 
-                <input type="hidden" name="quantite" id="modalQuantite" value="1">
+                <input type="hidden" name="quantite" id="cellierQuantite" value="1">
+                <input type="hidden" name="redirect_url" value="{{ url()->full() }}">
 
                 {{-- Actions --}}
                 <div class="flex gap-3">
